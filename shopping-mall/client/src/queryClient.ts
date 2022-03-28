0582567580f6bd4eb18c9a52e61b578a5fca5b0e
CHOI <tlacjd665@gmail.com>
2022-03-28 18:50:01 +0900

    서버 배포 완료 클라이언트-서버 연동 진행중...

--- shopping-mall/client/src/queryClient.ts
+++ shopping-mall/client/src/queryClient.ts
@@ -1,8 +1,6 @@
 import request, { RequestDocument } from 'graphql-request';
 import { QueryClient } from 'react-query';
 
-type AnyOBJ = { [key: string]: any };
-
 export const getClient = (() => {
   let client: QueryClient | null = null;
   return () => {
@@ -24,6 +22,7 @@ export const getClient = (() => {
 })();
 
 const BASE_URL = import.meta.env.VITE_SERVER_URL as string;
+console.log(BASE_URL);
 
 export const graphqlFetcher = (query: RequestDocument, variables = {}) =>
   request(`${BASE_URL}/graphql`, query, variables, {
